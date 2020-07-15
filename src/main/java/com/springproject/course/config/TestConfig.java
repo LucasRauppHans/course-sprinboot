b21893682290f4f255942d27d50a34634861af18
Lucas Hans <lucas_hans@hotmail.com>
2020-07-15 09:45:53 -0300

    OrderStatus ENUM

--- src/main/java/com/springproject/course/config/TestConfig.java
+++ src/main/java/com/springproject/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.springproject.course.config;
 
 import com.springproject.course.entities.Order;
 import com.springproject.course.entities.User;
+import com.springproject.course.entities.enums.OrderStatus;
 import com.springproject.course.repositories.OrderRepository;
 import com.springproject.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Grenn", "alex@gmail.com", "989898777", "12345");
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
     }
